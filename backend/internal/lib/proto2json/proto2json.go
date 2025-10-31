5f65b3b0b9f6d7d76ad40abc9a971e3ffc62ff96
rwrrioe <damir100609@gmail.com>
2025-10-31 19:48:28 +0500

    add lib and utilities

---
+++ backend/internal/lib/proto2json/proto2json.go
@@ -0,0 +1,16 @@
+package lib
+
+import (
+	"encoding/json"
+
+	pb "github.com/rwrrioe/pythia/backend/shared/gen/go/ocr"
+)
+
+func ConvertProto(resp *pb.OCRResponse) ([]byte, error) {
+	jsonStr, err := json.Marshal(resp)
+	if err != nil {
+		return nil, nil
+	}
+
+	return jsonStr, nil
+}
