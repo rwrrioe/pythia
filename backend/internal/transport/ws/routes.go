705cd9186bfd52ca236df75a8f8d49a5d7042e97
rwrrioe <damir100609@gmail.com>
2025-11-05 15:51:56 +0500

    add routing for websocket

--- backend/internal/transport/ws/routes.go
+++ backend/internal/transport/ws/routes.go
@@ -2,12 +2,32 @@ package ws
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rwrrioe/pythia/backend/internal/services"
 	ws_handlers "github.com/rwrrioe/pythia/backend/internal/transport/ws/handlers"
+	hub "github.com/rwrrioe/pythia/backend/internal/transport/ws/ws_hub"
 )
 
-func RegisterRoutes(r *gin.Engine, handler *ws_handlers.WebSocketOCRHandler) {
+type Handlers struct {
+	Services *services.Services
+	OCR      *ws_handlers.OCRHandler
+	Transl   *ws_handlers.TranslateHandler
+}
+
+func New(services *services.Services, ws *hub.WebSocketHub) *Handlers {
+	ocr := ws_handlers.NewOCRHandler(services.OCR, ws)
+	transl := ws_handlers.NewTranslateHandler(services.Translate, ws)
+
+	return &Handlers{
+		Services: services,
+		OCR:      ocr,
+		Transl:   transl,
+	}
+}
+
+func RegisterRoutes(r *gin.Engine, handlers *Handlers) {
 	ws := r.Group("/ws")
 	{
-		ws.GET("/ocr", handler.WebSocket)
+		ws.GET("/ocr", handlers.OCR.WebSocket)
+		ws.GET("/translate", handlers.Transl.WebSocket)
 	}
 }
