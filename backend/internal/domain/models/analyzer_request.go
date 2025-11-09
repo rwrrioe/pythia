c0bb664510bbc1563906dbef7db47376ff43fdee
rrwrrioe <damir100609@gmail.com>
2025-11-09 17:45:41 +0200

    refactore request

--- backend/internal/domain/models/analyzer_request.go
+++ backend/internal/domain/models/analyzer_request.go
@@ -1,10 +1,9 @@
 package models
 
 type AnalyzeRequest struct {
-	Text     []string `json:"text"`
-	Level    string   `json:"level"`
-	Lang     string   `json:"lang"`
-	Durating string   `json:"durating"`
-	Book     string   `json:"book"`
-	TaskID   string   `json:"taskID"`
+	Level    string `json:"level"`
+	Lang     string `json:"lang"`
+	Durating string `json:"durating"`
+	Book     string `json:"book"`
+	TaskID   string `json:"taskID"`
 }
