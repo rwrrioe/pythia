5448682e8d27fdc605d274e0fd43c662fa488c15
rrwrrioe <damir100609@gmail.com>
2025-11-17 17:05:46 +0200

    add learn service

---
+++ backend/internal/services/learn_service/learn.go
@@ -0,0 +1,55 @@
+package learn
+
+import (
+	"context"
+	"math/rand"
+
+	"github.com/rwrrioe/pythia/backend/internal/domain/entities"
+)
+
+type LearnService struct {
+	Optcount int
+}
+
+func NewLearnService(optcount int) *LearnService {
+	return &LearnService{Optcount: optcount}
+}
+
+func (s *LearnService) QuizTest(ctx context.Context, words *[]entities.UnknownWord) *[]entities.QuizQuestion {
+	var test []entities.QuizQuestion
+
+	for _, v := range *words {
+		opts := pickOptions(words, v.Word, s.Optcount)
+		questionDTO := entities.QuizQuestion{
+			Answer:   v.Word,
+			Question: v.Translation,
+			Options:  opts,
+		}
+		test = append(test, questionDTO)
+	}
+
+	return &test
+
+}
+
+func pickOptions(words *[]entities.UnknownWord, correct string, optcount int) []string {
+	var pool []string
+
+	for _, w := range *words {
+		if w.Word != correct {
+			pool = append(pool, w.Word)
+		}
+	}
+
+	rand.Shuffle(len(pool), func(i, j int) {
+		pool[i], pool[j] = pool[j], pool[i]
+	})
+
+	opts := make([]string, 0, optcount)
+	opts = append(opts, pool[:4]...)
+
+	insIdx := rand.Intn(len(opts))
+	opts[insIdx] = correct
+
+	return opts
+}
