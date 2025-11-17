f78be77caa1f465aa4bf6b0c9fb51e1f42ff654f
rrwrrioe <damir100609@gmail.com>
2025-11-17 17:06:54 +0200

    add quiz route

--- backend/internal/transport/rest/routes.go
+++ backend/internal/transport/rest/routes.go
@@ -11,17 +11,22 @@ import (
 type Handlers struct {
 	OCR      *rest_handlers.OCRHandler
 	Transl   *rest_handlers.TranslateHandler
+	FlCards  *rest_handlers.FlashCardsHandler
+	Learn    *rest_handlers.LearnHandler
 	Services *services.Services
 }
 
 func New(services *services.Services, hub *hub.WebSocketHub, storage *taskstorage.RedisTaskStorage) *Handlers {
 	ocr := rest_handlers.NewOCRHandler(services.OCR, hub, storage)
 	transl := rest_handlers.NewTranslateHandler(hub, services.Translate, storage)
-
+	flcards := rest_handlers.NewFlashCardsHandler(services.Cards, hub, storage)
+	learn := rest_handlers.NewLearnHandler(hub, services.Learn, storage)
 	return &Handlers{
 		Services: services,
 		OCR:      ocr,
 		Transl:   transl,
+		FlCards:  flcards,
+		Learn:    learn,
 	}
 }
 
@@ -31,5 +36,7 @@ func RegisterRoutes(r *gin.Engine, handlers *Handlers) {
 		api.POST("/upload", handlers.OCR.Upload)
 		api.POST("/translate", handlers.Transl.Translate)
 		api.POST("/translate/examples", handlers.Transl.WriteExamples)
+		api.POST("/flashcards", handlers.FlCards.FlashCards)
+		api.POST("/flashcards/tests/quiz", handlers.Learn.Quiz)
 	}
 }
