f07fca0908e15cefeb52a24281ba0d7cb4655c65
rrwrrioe <damir100609@gmail.com>
2026-01-14 22:17:28 +0500

    add grpc clients for sso and ocr

---
+++ backend/internal/clients/ocr/grpc/grpc.go
@@ -0,0 +1,74 @@
+package ocr_grpc_client
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	grpcretry "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
+	ocrv1 "github.com/rwrrioe/pythia_protos/gen/go/ocr"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Client struct {
+	api ocrv1.OCRServiceClient
+	log *slog.Logger
+}
+
+func New(
+	ctx context.Context,
+	log *slog.Logger,
+	addr string,
+	timeout time.Duration,
+	retriesCount int,
+) (*Client, error) {
+	const op = "ocr_grpc_client.New"
+
+	retryOpts := []grpcretry.CallOption{
+		grpcretry.WithCodes(codes.NotFound, codes.Aborted, codes.DeadlineExceeded),
+		grpcretry.WithMax(uint(retriesCount)),
+		grpcretry.WithPerRetryTimeout(timeout),
+	}
+
+	logOpts := []grpclog.Option{
+		grpclog.WithLogOnEvents(grpclog.PayloadReceived, grpclog.PayloadSent),
+	}
+
+	cc, err := grpc.NewClient(addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithChainUnaryInterceptor(
+			grpclog.UnaryClientInterceptor(InterceptorLogger(log), logOpts...),
+			grpcretry.UnaryClientInterceptor(retryOpts...),
+		))
+	if err != nil {
+		return nil, fmt.Errorf("%s:%w", op, err)
+	}
+
+	grpcClient := ocrv1.NewOCRServiceClient(cc)
+
+	return &Client{
+		api: grpcClient,
+	}, nil
+}
+
+func InterceptorLogger(l *slog.Logger) grpclog.Logger {
+	return grpclog.LoggerFunc(func(ctx context.Context, lvl grpclog.Level, msg string, fields ...any) {
+		l.Log(ctx, slog.Level(lvl), msg, fields...)
+	})
+}
+
+func (c *Client) ProcessImage(ctx context.Context, imageData []byte, lang string) ([]string, error) {
+	resp, err := c.api.Recognize(ctx, &ocrv1.OCRRequest{
+		ImageData: []byte(imageData),
+		Lang:      lang,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return resp.Text, nil
+}
