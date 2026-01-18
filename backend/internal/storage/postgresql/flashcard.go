65f112a1063f0751ff7f2d61afd813fa4d94c3d9
rrwrrioe <damir100609@gmail.com>
2026-01-18 10:27:50 +0200

    add flashcard repo

--- backend/internal/storage/postgresql/flashcard.go
+++ backend/internal/storage/postgresql/flashcard.go
@@ -1,7 +1,13 @@
 package postgresql
 
 import (
+	"context"
+	"errors"
+	"fmt"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/rwrrioe/pythia/backend/internal/auth"
+	"github.com/rwrrioe/pythia/backend/internal/storage/models"
 )
 
 type FlashCardStorage struct {
@@ -12,4 +18,43 @@ func NewFlashcardRepo(conn *pgx.Conn) *FlashCardStorage {
 	return &FlashCardStorage{
 		conn: conn,
 	}
+}
+
+func (s *FlashCardStorage) ListByDeck(ctx context.Context, deckId int) ([]models.FlashCard, error) {
+	const op = "Storage.FlashCardStorage.ListByDeck"
+
+	var flashcards []models.FlashCard
+	uid, ok := auth.UIDFromContext(ctx)
+	if !ok {
+		return nil, ErrUserNotFound
+	}
+
+	rows, err := s.conn.Query(ctx, `SELECT f.id, f.word, f.trans, f.description, l.language 
+										FROM decks_flashcards df 
+										JOIN flashcards f ON df.flashcard_id = f.id
+										JOIN languages l ON f.lang_id = l.id
+										WHERE f.user_id = $1 AND df.deck_id = $2
+										`, uid, deckId)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, ErrDeckNotFound
+		}
+		return nil, fmt.Errorf("%s:%w", op, err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var fc models.FlashCard
+
+		if err = rows.Scan(&fc); err != nil {
+			return nil, fmt.Errorf("%s:%w", op, err)
+		}
+
+		flashcards = append(flashcards, fc)
+	}
+
+	if rows.Err() != nil {
+		return nil, fmt.Errorf("%s:%w", op, err)
+	}
+	return flashcards, nil
 }
