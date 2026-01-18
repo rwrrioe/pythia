19e037d330eb776e9302c62ca2c700b55994697e
rrwrrioe <damir100609@gmail.com>
2026-01-18 09:09:23 +0200

    add sso and ocr grpc clients

--- backend/internal/clients/ocr/grpc/grpc.go
+++ backend/internal/clients/ocr/grpc/grpc.go
@@ -2,16 +2,10 @@ package ocr_grpc_client
 
 import (
 	"context"
-	"fmt"
 	"log/slog"
-	"time"
 
-	grpclog "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
-	grpcretry "github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/retry"
 	ocrv1 "github.com/rwrrioe/pythia_protos/gen/go/ocr"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 type Client struct {
@@ -21,44 +15,15 @@ type Client struct {
 
 func New(
 	ctx context.Context,
+	cc *grpc.ClientConn,
 	log *slog.Logger,
-	addr string,
-	timeout time.Duration,
-	retriesCount int,
-) (*Client, error) {
-	const op = "ocr_grpc_client.New"
-
-	retryOpts := []grpcretry.CallOption{
-		grpcretry.WithCodes(codes.NotFound, codes.Aborted, codes.DeadlineExceeded),
-		grpcretry.WithMax(uint(retriesCount)),
-		grpcretry.WithPerRetryTimeout(timeout),
-	}
-
-	logOpts := []grpclog.Option{
-		grpclog.WithLogOnEvents(grpclog.PayloadReceived, grpclog.PayloadSent),
-	}
-
-	cc, err := grpc.NewClient(addr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithChainUnaryInterceptor(
-			grpclog.UnaryClientInterceptor(InterceptorLogger(log), logOpts...),
-			grpcretry.UnaryClientInterceptor(retryOpts...),
-		))
-	if err != nil {
-		return nil, fmt.Errorf("%s:%w", op, err)
-	}
-
+) *Client {
 	grpcClient := ocrv1.NewOCRServiceClient(cc)
 
 	return &Client{
 		api: grpcClient,
-	}, nil
-}
-
-func InterceptorLogger(l *slog.Logger) grpclog.Logger {
-	return grpclog.LoggerFunc(func(ctx context.Context, lvl grpclog.Level, msg string, fields ...any) {
-		l.Log(ctx, slog.Level(lvl), msg, fields...)
-	})
+		log: log,
+	}
 }
 
 func (c *Client) ProcessImage(ctx context.Context, imageData []byte, lang string) ([]string, error) {
--- backend/internal/clients/sso/grpc/grpc.go
+++ backend/internal/clients/sso/grpc/grpc.go
@@ -1,1 +1,27 @@
 package sso_grpc_client
+
+import (
+	"context"
+	"log/slog"
+
+	ssov1 "github.com/rwrrioe/sso_protos/gen/go/sso"
+	"google.golang.org/grpc"
+)
+
+type Client struct {
+	api ssov1.AuthClient
+	log *slog.Logger
+}
+
+func New(
+	ctx context.Context,
+	cc *grpc.ClientConn,
+	log *slog.Logger,
+) *Client {
+	grpcClient := ssov1.NewAuthClient(cc)
+
+	return &Client{
+		api: grpcClient,
+		log: log,
+	}
+}
