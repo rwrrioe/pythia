6dab9dfb89a540f7d65f60f558b59888654ca261
rrwrrioe <damir100609@gmail.com>
2026-01-19 18:35:57 +0200

    refactor intefaces structure

--- backend/migrations/1_init.up.sql
+++ backend/migrations/1_init.up.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS languages
 CREATE TABLE IF NOT EXISTS levels
 (
     id int,
-    level VARCHAR(2)
+    level VARCHAR(50)
     )
 
 CREATE TABLE IF NOT EXISTS users
@@ -28,11 +28,16 @@ CREATE INDEX IF NOT EXISTS idx_id ON users(id)
 CREATE TABLE IF NOT EXISTS sessions
 (
     id int,
+    name varchar(100),
     user_id int,
+    status varchar(50),
+    lang_id int,
     started_at timestamp,
     ended_at timestamp,
-    accuracy float
-)
+    accuracy float,
+
+    CONSTRAINT fk_sessions_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
+    );
 CREATE INDEX IF NOT EXISTS idx_session_id ON decks(id)
 
 CREATE TABLE IF NOT EXISTS words
@@ -69,11 +74,11 @@ CREATE TABLE IF NOT EXISTS decks
 (
     id int
     user_id int,
-    started_at timestamp,
-    finished_at timestamp
+    session_id int
 
     CONSTRAINT pk_decks PRIMARY KEY (id),
     CONSTRAINT decks_users FOREIGN KEY(user_id) REFERENCES users(id),
+    CONSTRAINT decks_sessions FOREIGN KEY(session_id) REFERENCES sessions(id),
     );
 CREATE INDEX IF NOT EXISTS idx_deck_id ON decks(id)
 
@@ -87,15 +92,7 @@ CREATE TABLE IF NOT EXISTS decks_flashcards
     CONSTRAINT decks_flashcards_flashcards FOREIGN KEY(flashcard_id) REFERENCES flashcards(id),
 
     PRIMARY KEY (deck_id, flashcard_id)
-    );
+);
 
-CREATE TABLE IF NOT EXISTS sessions_decks
-(
-    session_id int,
-    deck_id int
 
-    CONSTRAINT sessions_decks_sessions FOREIGN KEY(session_id) REFERENCES sessions(id),
-    CONSTRAINT sessions_decks_decks FOREIGN KEY(deck_id) REFERENCES decks(id),
-    PRIMARY KEY (session_id, deck_id)
-    )
 
