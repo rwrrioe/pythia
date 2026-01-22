967b16c4323df61876463b548f5dab9092ad2f17
rrwrrioe <damir100609@gmail.com>
2026-01-22 17:12:45 +0200

    add deck/flashcard already exist error

--- backend/internal/services/errors/errors.go
+++ backend/internal/services/errors/errors.go
@@ -1,9 +1,10 @@
-package service
+package service_errors
 
 import "errors"
 
 var (
-	ErrEmptyText       = errors.New("text is empty")
-	ErrNoWords         = errors.New("no words")
-	ErrSessionNotFound = errors.New("session not found")
+	ErrEmptyText              = errors.New("text is empty")
+	ErrNoWords                = errors.New("no words")
+	ErrSessionNotFound        = errors.New("session not found")
+	ErrSessionAlreadyFinished = errors.New("session already finished")
 )
