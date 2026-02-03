97e15c8ef5dde07ec0a807af5dd72d1001122b7c
rrwrrioe <damir100609@gmail.com>
2026-02-03 17:06:35 +0200

    refactor ws hub to session structure

--- backend/internal/transport/ws/ws_hub/ws_hub.go
+++ backend/internal/transport/ws/ws_hub/ws_hub.go
@@ -1,43 +1,48 @@
 package ws_hub
 
 import (
-	"log"
 	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type WebSocketHub struct {
-	mu      sync.Mutex
-	clients map[string]*websocket.Conn
+	mu       sync.Mutex
+	sessions map[int]map[*websocket.Conn]struct{}
 }
 
 func NewWebSocketHub() *WebSocketHub {
-	return &WebSocketHub{clients: make(map[string]*websocket.Conn)}
+	return &WebSocketHub{sessions: make(map[int]map[*websocket.Conn]struct{})}
 }
 
-func (h *WebSocketHub) AddClient(id string, conn *websocket.Conn) {
-	h.mu.Lock()
-	h.clients[id] = conn
-	h.mu.Unlock()
-}
-
-func (h *WebSocketHub) Notify(id string, payload interface{}) {
+func (h *WebSocketHub) Add(sessionId int, conn *websocket.Conn) {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	if conn, ok := h.clients[id]; ok {
-		if err := conn.WriteJSON(payload); err != nil {
-			log.Printf("Failed to send WS message to %s: %v", id, err)
+	if h.sessions[sessionId] == nil {
+		h.sessions[sessionId] = make(map[*websocket.Conn]struct{})
+	}
 
-			conn.Close()
-			delete(h.clients, id)
-		}
+	h.sessions[sessionId][conn] = struct{}{}
+}
+
+func (h *WebSocketHub) Notify(sessionId int, payload interface{}) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	for conn, _ := range h.sessions[sessionId] {
+		_ = conn.WriteJSON(payload)
 	}
 }
 
-func (h *WebSocketHub) RemoveClient(id string) {
+func (h *WebSocketHub) Remove(sessionId int, conn *websocket.Conn) {
 	h.mu.Lock()
-	delete(h.clients, id)
-	h.mu.Unlock()
+	defer h.mu.Unlock()
+
+	if conns, ok := h.sessions[sessionId]; ok {
+		delete(conns, conn)
+		if len(conns) == 0 {
+			delete(h.sessions, sessionId)
+		}
+	}
 }
