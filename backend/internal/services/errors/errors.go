6694448601b52cf236a6d6279a71dbae6b7171fe
rrwrrioe <damir100609@gmail.com>
2026-02-03 17:08:17 +0200

    add new errors

--- backend/internal/services/errors/errors.go
+++ backend/internal/services/errors/errors.go
@@ -7,4 +7,7 @@ var (
 	ErrNoWords                = errors.New("no words")
 	ErrSessionNotFound        = errors.New("session not found")
 	ErrSessionAlreadyFinished = errors.New("session already finished")
+	ErrTaskNotFound           = errors.New("task not found")
+	ErrUnauthorized           = errors.New("user is unauthorized")
+	ErrForbidden              = errors.New("access forbidden")
 )
