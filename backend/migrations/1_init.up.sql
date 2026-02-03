eeb30b17b1824a2c9d87776f8bb60eecad0b6c67
rrwrrioe <damir100609@gmail.com>
2026-02-03 17:10:26 +0200

    fix migration scheme bugs

--- backend/migrations/1_init.up.sql
+++ backend/migrations/1_init.up.sql
@@ -1,20 +1,24 @@
 CREATE TABLE IF NOT EXISTS languages
 (
     id int,
-    language VARCHAR(50)
-    )
+    language VARCHAR(50),
+
+    CONSTRAINT pk_languages PRIMARY KEY(id)
+    );
 
 CREATE TABLE IF NOT EXISTS levels
 (
     id int,
-    level VARCHAR(50)
-    )
+    level VARCHAR(50),
+
+    CONSTRAINT pk_levels PRIMARY KEY(id)
+    );
 
 CREATE TABLE IF NOT EXISTS users
 (
     id int,
     email varchar(100),
-    name string NOT NULL,
+    name varchar(100),
     level_id int NOT NULL,
     lang_id int NOT NULL,
     words_per_day INT NOT NULL,
@@ -23,7 +27,7 @@ CREATE TABLE IF NOT EXISTS users
     CONSTRAINT fk_users_levels FOREIGN KEY(level_id) REFERENCES levels(id),
     CONSTRAINT fk_users_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
     );
-CREATE INDEX IF NOT EXISTS idx_id ON users(id)
+CREATE INDEX IF NOT EXISTS idx_id ON users(id);
 
 CREATE TABLE IF NOT EXISTS sessions
 (
@@ -36,51 +40,38 @@ CREATE TABLE IF NOT EXISTS sessions
     ended_at timestamp,
     accuracy float,
 
+    CONSTRAINT pk_sessions PRIMARY KEY(id),
     CONSTRAINT fk_sessions_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
     );
-CREATE INDEX IF NOT EXISTS idx_session_id ON decks(id)
-
-CREATE TABLE IF NOT EXISTS words
-(
-    id int
-    user_id int,
-    word varchar(100),
-    transl varchar(100),
-    description varchar(100),
-    lang_id int,
-
-    CONSTRAINT pk_words PRIMARY KEY,
-    CONSTRAINT  words_users FOREIGN KEY(user_id) REFERENCES users(id),
-    CONSTRAINT fk_words_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
-    );
-CREATE INDEX IF NOT EXISTS idx_word_id ON words(id)
+CREATE INDEX IF NOT EXISTS idx_session_id ON sessions(id);
 
 CREATE TABLE IF NOT EXISTS flashcards
 (
-    id int
+    id int,
     user_id int,
     word varchar(100),
     transl varchar(100),
-    description varchar(100),
     lang_id int,
 
+    CONSTRAINT uq_flashcards_user_word_lang UNIQUE (user_id, word, lang_id),
     CONSTRAINT pk_flashcards PRIMARY KEY (id),
     CONSTRAINT  flashcards_users FOREIGN KEY(user_id) REFERENCES users(id),
     CONSTRAINT fk_words_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
     );
-CREATE INDEX IF NOT EXISTS idx_flashcard_id ON flaschards(id)
+CREATE INDEX IF NOT EXISTS idx_flashcard_id ON flashcards(id);
 
 CREATE TABLE IF NOT EXISTS decks
 (
-    id int
+    id int,
     user_id int,
-    session_id int
+    session_id int,
 
+    CONSTRAINT uq_decks_user_session UNIQUE (user_id, session_id),
     CONSTRAINT pk_decks PRIMARY KEY (id),
     CONSTRAINT decks_users FOREIGN KEY(user_id) REFERENCES users(id),
-    CONSTRAINT decks_sessions FOREIGN KEY(session_id) REFERENCES sessions(id),
+    CONSTRAINT decks_sessions FOREIGN KEY(session_id) REFERENCES sessions(id)
     );
-CREATE INDEX IF NOT EXISTS idx_deck_id ON decks(id)
+CREATE INDEX IF NOT EXISTS idx_deck_id ON decks(id);
 
 
 CREATE TABLE IF NOT EXISTS decks_flashcards
@@ -92,7 +83,7 @@ CREATE TABLE IF NOT EXISTS decks_flashcards
     CONSTRAINT decks_flashcards_flashcards FOREIGN KEY(flashcard_id) REFERENCES flashcards(id),
 
     PRIMARY KEY (deck_id, flashcard_id)
-);
+    );
 
 
 
