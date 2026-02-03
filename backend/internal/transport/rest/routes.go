9e652a7414c7632c303863917ebbbf046ec21300
rrwrrioe <damir100609@gmail.com>
2026-02-03 17:08:07 +0200

    refactor to session based structure, add new routes

--- backend/internal/transport/rest/routes.go
+++ backend/internal/transport/rest/routes.go
@@ -2,6 +2,7 @@ package rest
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rwrrioe/pythia/backend/internal/auth/authn"
 	"github.com/rwrrioe/pythia/backend/internal/services"
 	taskstorage "github.com/rwrrioe/pythia/backend/internal/storage/redis/task_storage"
 	rest_handlers "github.com/rwrrioe/pythia/backend/internal/transport/rest/handlers"
@@ -9,34 +10,61 @@ import (
 )
 
 type Handlers struct {
-	OCR      *rest_handlers.OCRHandler
-	Transl   *rest_handlers.TranslateHandler
-	FlCards  *rest_handlers.FlashCardsHandler
-	Learn    *rest_handlers.LearnHandler
-	Services *services.Services
+	authHandler       *rest_handlers.AuthHandler
+	ocrHandler        *rest_handlers.OCRHandler
+	translateHandler  *rest_handlers.TranslateHandler
+	learnHandler      *rest_handlers.LearnHandler
+	sessionHandler    *rest_handlers.SessionHandler
+	flashcardsHandler *rest_handlers.FlashCardsHandler
+	statsHandler      *rest_handlers.StatsHandler
 }
 
-func New(services *services.Services, hub *hub.WebSocketHub, storage *taskstorage.RedisTaskStorage) *Handlers {
-	ocr := rest_handlers.NewOCRHandler(services.OCR, hub, storage)
-	transl := rest_handlers.NewTranslateHandler(hub, services.Translate, storage)
-	flcards := rest_handlers.NewFlashCardsHandler(services.Cards, hub, storage)
-	learn := rest_handlers.NewLearnHandler(hub, services.Learn, storage)
+func New(session *service.SessionService, stats *service.StatsService, sso authn.SSOService, ws *hub.WebSocketHub, storage *taskstorage.RedisStorage) *Handlers {
+
+	ocr := rest_handlers.NewOCRHandler(storage, ws, session)
+	transl := rest_handlers.NewTranslateHandler(storage, ws, session)
+	flCards := rest_handlers.NewFlashCardsHandler(storage, ws, session)
+	learn := rest_handlers.NewLearnHandler(storage, ws, session)
+	ss := rest_handlers.NewSessionHandler(storage, ws, session)
+	authH := rest_handlers.NewAuthHandler(sso)
+	statsH := rest_handlers.NewStatsHandler(stats)
+
 	return &Handlers{
-		Services: services,
-		OCR:      ocr,
-		Transl:   transl,
-		FlCards:  flcards,
-		Learn:    learn,
+		ocrHandler:        ocr,
+		translateHandler:  transl,
+		learnHandler:      learn,
+		sessionHandler:    ss,
+		authHandler:       authH,
+		flashcardsHandler: flCards,
+		statsHandler:      statsH,
 	}
 }
 
-func RegisterRoutes(r *gin.Engine, handlers *Handlers) {
+func RegisterRoutes(r *gin.Engine, handlers *Handlers, auth gin.HandlerFunc, requireAuth gin.HandlerFunc) {
 	api := r.Group("/api")
+	api.Use(auth)
+
+	protected := api.Group("")
+	protected.Use(requireAuth)
+	protected.GET("/dashboard", handlers.statsHandler.Dashboard)
+
+	session := api.Group("/session")
+	session.POST("/new", handlers.sessionHandler.NewSession)
+
+	sessionProtected := session.Group("")
+	sessionProtected.Use(requireAuth)
 	{
-		api.POST("/upload", handlers.OCR.Upload)
-		api.POST("/translate", handlers.Transl.Translate)
-		api.POST("/translate/examples", handlers.Transl.WriteExamples)
-		api.POST("/flashcards", handlers.FlCards.FlashCards)
-		api.POST("/flashcards/tests/quiz", handlers.Learn.Quiz)
+		sessionProtected.POST("/:sessionId/upload", handlers.ocrHandler.Upload)
+		sessionProtected.POST("/:sessionId/task/:taskId/translate", handlers.translateHandler.Translate)
+		sessionProtected.PATCH("/:sessionId/end", handlers.sessionHandler.EndSession)
+		sessionProtected.GET("/:sessionId/learn/flashcards", handlers.flashcardsHandler.FlashCards)
+		sessionProtected.GET("/:sessionId/learn/quiz", handlers.learnHandler.Quiz)
+		sessionProtected.POST("/:sessionId/summary", handlers.sessionHandler.SessionSummary)
+	}
+
+	public := r.Group("/api/auth")
+	{
+		public.POST("/login", handlers.authHandler.Login)
+		public.POST("/register", handlers.authHandler.Register)
 	}
 }
