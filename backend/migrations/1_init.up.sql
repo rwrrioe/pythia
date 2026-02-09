19f17d747054ad0141d9c1e551544325f09b12f8
rrwrrioe <damir100609@gmail.com>
2026-02-09 15:49:55 +0200

    fix migration

--- backend/migrations/1_init.up.sql
+++ backend/migrations/1_init.up.sql
@@ -1,89 +1,105 @@
-CREATE TABLE IF NOT EXISTS languages
-(
-    id int,
-    language VARCHAR(50),
+BEGIN
 
-    CONSTRAINT pk_languages PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS languages (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
     );
 
-CREATE TABLE IF NOT EXISTS levels
-(
-    id int,
-    level VARCHAR(50),
-
-    CONSTRAINT pk_levels PRIMARY KEY(id)
+CREATE TABLE IF NOT EXISTS levels (
+    id INT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL UNIQUE
     );
 
-CREATE TABLE IF NOT EXISTS users
-(
-    id int,
-    email varchar(100),
-    name varchar(100),
-    level_id int NOT NULL,
-    lang_id int NOT NULL,
+
+
+CREATE TABLE IF NOT EXISTS users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    level_id INT NOT NULL,
+    lang_id INT NOT NULL,
     words_per_day INT NOT NULL,
 
-    CONSTRAINT pk_users PRIMARY KEY(id),
-    CONSTRAINT fk_users_levels FOREIGN KEY(level_id) REFERENCES levels(id),
-    CONSTRAINT fk_users_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
-    );
-CREATE INDEX IF NOT EXISTS idx_id ON users(id);
+    CONSTRAINT fk_users_levels
+    FOREIGN KEY (level_id) REFERENCES levels(id),
 
-CREATE TABLE IF NOT EXISTS sessions
-(
-    id int,
-    name varchar(100),
-    user_id int,
-    status varchar(50),
-    lang_id int,
-    started_at timestamp,
-    ended_at timestamp,
-    accuracy float,
-
-    CONSTRAINT pk_sessions PRIMARY KEY(id),
-    CONSTRAINT fk_sessions_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
-    );
-CREATE INDEX IF NOT EXISTS idx_session_id ON sessions(id);
-
-CREATE TABLE IF NOT EXISTS flashcards
-(
-    id int,
-    user_id int,
-    word varchar(100),
-    transl varchar(100),
-    lang_id int,
-
-    CONSTRAINT uq_flashcards_user_word_lang UNIQUE (user_id, word, lang_id),
-    CONSTRAINT pk_flashcards PRIMARY KEY (id),
-    CONSTRAINT  flashcards_users FOREIGN KEY(user_id) REFERENCES users(id),
-    CONSTRAINT fk_words_languages FOREIGN KEY(lang_id) REFERENCES languages(id)
-    );
-CREATE INDEX IF NOT EXISTS idx_flashcard_id ON flashcards(id);
-
-CREATE TABLE IF NOT EXISTS decks
-(
-    id int,
-    user_id int,
-    session_id int,
-
-    CONSTRAINT uq_decks_user_session UNIQUE (user_id, session_id),
-    CONSTRAINT pk_decks PRIMARY KEY (id),
-    CONSTRAINT decks_users FOREIGN KEY(user_id) REFERENCES users(id),
-    CONSTRAINT decks_sessions FOREIGN KEY(session_id) REFERENCES sessions(id)
-    );
-CREATE INDEX IF NOT EXISTS idx_deck_id ON decks(id);
-
-
-CREATE TABLE IF NOT EXISTS decks_flashcards
-(
-    deck_id int,
-    flashcard_id int,
-
-    CONSTRAINT decks_flashcards_decks FOREIGN KEY(deck_id) REFERENCES decks(id),
-    CONSTRAINT decks_flashcards_flashcards FOREIGN KEY(flashcard_id) REFERENCES flashcards(id),
-
-    PRIMARY KEY (deck_id, flashcard_id)
+    CONSTRAINT fk_users_languages
+    FOREIGN KEY (lang_id) REFERENCES languages(id)
     );
 
 
 
+CREATE TABLE IF NOT EXISTS sessions (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    user_id BIGINT NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    lang_id INT NOT NULL,
+    started_at TIMESTAMP NOT NULL,
+    ended_at TIMESTAMP,
+    accuracy DOUBLE PRECISION,
+
+    CONSTRAINT fk_sessions_users
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_sessions_languages
+    FOREIGN KEY (lang_id) REFERENCES languages(id)
+    );
+
+CREATE TABLE IF NOT EXISTS flashcards (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    word VARCHAR(100) NOT NULL,
+    transl VARCHAR(100) NOT NULL,
+    lang_id INT NOT NULL,
+
+    CONSTRAINT uq_flashcards_user_word_lang
+    UNIQUE (user_id, word, lang_id),
+
+    CONSTRAINT fk_flashcards_users
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_flashcards_languages
+    FOREIGN KEY (lang_id) REFERENCES languages(id)
+    );
+
+CREATE TABLE IF NOT EXISTS decks (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    session_id BIGINT NOT NULL,
+    CONSTRAINT uq_decks_user_session
+    UNIQUE (user_id, session_id),
+
+    CONSTRAINT fk_decks_users
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_decks_sessions
+    FOREIGN KEY (session_id) REFERENCES sessions(id) ON DELETE CASCADE
+    );
+
+
+CREATE TABLE IF NOT EXISTS decks_flashcards (
+    deck_id BIGINT NOT NULL,
+    flashcard_id BIGINT NOT NULL,
+
+    PRIMARY KEY (deck_id, flashcard_id),
+    CONSTRAINT fk_df_decks
+    FOREIGN KEY (deck_id) REFERENCES decks(id) ON DELETE CASCADE,
+
+    CONSTRAINT fk_df_flashcards
+    FOREIGN KEY (flashcard_id) REFERENCES flashcards(id) ON DELETE CASCADE
+    );
+
+INSERT INTO languages (id, name)
+VALUES
+    (1, 'english'),
+    (2, 'deutsch')
+    ON CONFLICT DO NOTHING;
+
+INSERT INTO levels (id, name)
+VALUES
+        (1,'A1'),
+        (2, 'A2')
+    ON CONFLICT DO NOTHING;
+
+COMMIT;
